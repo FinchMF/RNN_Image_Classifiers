76d00635449761c513ca98f7110d5efaa6bba4ed
Matthew Finch <finchmf@Matthews-MBP.home>
2020-07-22 23:23:12 -0400

    2020-7-22-1

--- nn_config.py
+++ nn_config.py
@@ -8,13 +8,13 @@ def set_data_params(num_train):
     params = {}
     params['batch_size'] = 100
     params['n_iters'] = 6000
-    params['num_epochs'] = int(params['n_iters'] / num_train /params['batch_size'])
+    params['num_epochs'] = int(params['n_iters'] / (num_train /params['batch_size']))
     return params
 
 
 def set_network_params():
     params = {}
-    params['input_dim'] = 30
+    params['input_dim'] = 28
     params['hidden_dim'] = 128
     params['n_layer'] = 2
     params['output_dim'] = 10
